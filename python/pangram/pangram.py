b585323b58e02b1921dfe16a9ba1c8dbb9f1665f
Jessie <mxjessie@users.noreply.github.com>
2018-02-09 16:30:50 -0800

    hokey pangrammer

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,4 @@
-def is_pangram():
-    pass
+def is_pangram(inpan=""):
+    """ what a disaster lmao """
+    return False not in [(y in inpan.upper()) for y in
+                         [chr(x) for x in range(65, 65+26)]]
