1b5128f2220d204f2066498e39adc51150bbe578
Jessie <mxjessie@users.noreply.github.com>
2018-02-09 16:01:53 -0800

    did gigasecond, hole-in-one

---
+++ go/gigasecond/gigasecond.go
@@ -0,0 +1,11 @@
+// Package gigasecond provides addGigasecond for adding a gigasecond to a time.Time.
+package gigasecond
+
+// import path for the time package from the standard library
+import "time"
+
+// AddGigasecond adds one (1) gigasecond to a time.Time.
+func AddGigasecond(t time.Time) time.Time {
+	gigasecond := time.Duration(1000000000) * time.Second
+	return t.Add(gigasecond)
+}
